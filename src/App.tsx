c8d636b4a49c977cf8b4277aa676fac921002281
Alexander Jessen <alecjessen@gmail.com>
2023-06-29 19:59:28 -0700

    Creating Basic Loader Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import react from 'react';
-// import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Loader from './components/Loader'
-import * as THREE from '@react-three/fiber';
-// import Example from './components/Example';
-// import ButtonExample from './components/ButtonExample';
-
-
+import Loader from './components/Loader.jsx'
 
 
 export default function App() {
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,27 +1,64 @@
-import React from 'react'
-import { OrbitControls } from "@react-three/drei"
-import { useRef, useState } from 'react';
-import { useSpring, animated } from '@react-spring/three'
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF, PerspectiveCamera } from "@react-three/drei";
+import { MeshToonMaterial } from "three";
+
+export default function Loader(props) {
+  // color, material, speed, scale//100
+
+const scale = props.scale/100 || 0.01
+const color = props.color || 'cyan'
+const material = props.material || MeshToonMaterial
+const speed = props.speed || 1
+
+const materialAll = new material({color: color});
+const loader = useRef();
+
+  useFrame((_state, delta) => {
+    loader.current.rotation.y += delta * speed;
+  });
 
 
-export default function Loader() {
-  
-  const [active, setActive] = useState(false);
-
-  const cube = useRef();
-
-  const { scale } = useSpring({ scale: active ? 1.5 : 1 })
-
-
+  const { nodes } = useGLTF("/loader.gltf");
   return (
-    <>
-    <OrbitControls makeDefault={ true }/>
-    <animated.mesh scale={ 2 } onClick={handleClick} ref={cube}>
-        <boxGeometry />
-        <meshBasicMaterial wireframe={ false } color={ 'red' } />
-    </animated.mesh>
-    </>
-  )
+    <group ref={loader}>
+      <group scale={scale}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Pentagon_7.geometry}
+          position={[-0.019, 1.957, -61.235]}
+          material={materialAll}
+        />
+        <meshBasicMaterial color = {'red'}/>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Pentagon_6.geometry}
+          position={[-0.019, 1.957, 66.482]}
+          material={materialAll}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Pentagon.geometry}
+          material={materialAll}
+        />
+        <directionalLight
+          intensity={2}
+          rotation={[-0.506, 0.629, 0.756]}
+        />
+        <PerspectiveCamera
+          makeDefault={false}
+          far={100000}
+          near={70}
+          fov={45}
+          position={[0, 0, -1000]}
+          rotation={[-Math.PI, 0, Math.PI]}
+        />
+      </group>
+    </group>
+  );
 }
 
-
+useGLTF.preload("/loader.gltf");
