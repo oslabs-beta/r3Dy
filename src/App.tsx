880e900e5214b97df92033293d9d090757dd870c
bryan trang <bryanltrang@gmail.com>
2023-07-01 15:15:55 -0700

    more progress on text field

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
   
   return (
     <Canvas>
+      {/* ADD COMPONENT HERE TO TEST */}
       <TextField />
     </Canvas>
   )
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,6 +1,6 @@
-import { useState, useRef, useEffect, SyntheticEvent } from 'react'
-import { Text, OrbitControls, Html } from '@react-three/drei'
-import * as THREE from 'three'
+import { useState, useRef } from 'react'
+import { Text, OrbitControls, Html, RoundedBox } from '@react-three/drei'
+import { Mesh, MeshStandardMaterial } from 'three'
 
 
 type TextFieldProps = {
@@ -13,35 +13,25 @@ type TextFieldProps = {
 }
 
 const inputStyles = {
-    width: '650px',
+    width: '750px',
     height: '200px',
     opacity: 0,
 }
 
-const TextField: React.FC<TextFieldProps> = ({
-    color = 'black', 
-    // width = 2, 
-    // height = 1, 
-    // backgroundColor = 'black', 
-    // backgroundOpacity = 0.3, 
-    // padding,
-}) => {
+const TextField = ({color}: TextFieldProps): JSX.Element => {
 
   const [type, setType] = useState('Hello r3Dy');
-//   const [typeState, setTypeState] = useState(false);
-//   const textRef = useRef<HTMLDivElement | null>(null);
-  const planeRef: object = useRef<THREE.MeshBasicMaterial>(null);
 
-  const planeHeight = 1.5;
-  const planeWidth = 6;
+  const planeRef = useRef<MeshStandardMaterial>(null!);
+  const boxRef = useRef<Mesh>(null!);
 
+  const boxHeight = 1.5;
+  const boxWidth = 10;
+  const boxDepth = 0.2;
 
-//   useEffect(() => {
-//     console.log(textRef.current);
-//   },[])
 
 const handleType = (e: React.FormEvent<HTMLInputElement>) => {
-    console.log('e:',e)
+    // console.log('e:',e)
     setType(e.currentTarget.value);
 }
 
@@ -49,25 +39,33 @@ const handleFocus = (): void => {
     console.log(planeRef);
     planeRef.current.color.set('lightblue');
 }
-const handleUnfocus = (): void => {
-    planeRef.current.color.set('lightgrey');
+const handleUnfocused = (): void => {
+    planeRef.current.color.set('#D5DEEA');
 }
 
   return (
     <>
     <OrbitControls />
+    <directionalLight
+          intensity={0.7}
+          rotation={[-0.888, 0.368, 1.286]}
+          position={[400, 320, 230]}
+        />
+    <hemisphereLight intensity={0.75} color="#eaeaea" />
         <group>
-            <mesh>
+            <mesh castShadow receiveShadow>
                 <Html center >
-                    <input type="text" style={inputStyles} onChange={handleType} onFocus={handleFocus} onBlur={handleUnfocus} value={type}></input>
+                    <input type="text" style={inputStyles} onChange={handleType} onFocus={handleFocus} onBlur={handleUnfocused} value={type}></input>
                 </Html>
-                <Text position-x='-2.5' anchorX='left' color={ color } font='fonts/Inter-Bold.ttf'>{ type }</Text>
+                <Text fontSize={0.5} castShadow position-x='-4.5' anchorX='left' color={ color ? color : 'black'} font='fonts/Inter-Bold.ttf' maxWidth={boxWidth - 0.5} textAlign='left' overflowWrap='break-word'>{ type }</Text>
             </mesh>
-            <mesh position-z={ -.3 } >
-                <planeGeometry args={ [planeWidth, planeHeight] }/>
-                <meshBasicMaterial side={THREE.DoubleSide} color='lightgrey' ref={ planeRef }/>
+            <mesh position-z={ -.3 }  castShadow receiveShadow ref = { boxRef }>
+            <RoundedBox args={ [boxWidth, boxHeight, boxDepth] } castShadow receiveShadow smoothness={4}> 
+                <meshStandardMaterial color='#D5DEEA' ref={ planeRef } />
+            </RoundedBox>
             </mesh>
         </group>
+
     </>
   )
 }
