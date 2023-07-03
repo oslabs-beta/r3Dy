4da0be46bb36f40320ab48a85c35542451f868c2
Alexander Jessen <alecjessen@gmail.com>
2023-07-03 14:30:26 -0700

    safety commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import Loader from './components/Loader'
+import TextField from './components/TextField'
 import { OrbitControls } from '@react-three/drei'
 import { MeshLambertMaterial, MeshMatcapMaterial, MeshPhysicalMaterial, MeshToonMaterial } from 'three'
 
@@ -7,7 +8,7 @@ export default function App() {
   
   return (
   <Canvas>
-
+<TextField/>
   </Canvas>
   )
 }
