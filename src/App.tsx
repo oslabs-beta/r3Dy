04ed3aa56dc7850fb2082c6acab5e5b366342eb2
18gwoo <garrettwoogs@yahoo.com>
2023-07-10 13:06:43 -0700

    finished typescrip for slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 
 //IMPORT COMPONENTS
+import React, { useState, useEffect } from 'react'
 import Button from './components/Button';
 import TextField from './components/TextField';
 import Loader from './components/Loader';
@@ -12,9 +13,20 @@ import Switch from './components/Switch';
 
 export default function App() {
 
+  const [slider, setSlider] = useState(0);
   return (
-    <Canvas shadows>
-  <Switch />
+    <Canvas 
+    camera = { {
+      near:.1,
+      far:200,
+      position: [0, 1, 8]
+    } }
+    >
+      <Slider 
+      onChange={setSlider}
+      // maxValue={10}
+      // steps={2}
+      value={slider} />
     </Canvas>
   )
 }
