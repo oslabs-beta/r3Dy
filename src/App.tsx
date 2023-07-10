bd21741676f852d77050a5abd4436ac73c3b6d29
Alexander Jessen <alecjessen@gmail.com>
2023-07-09 20:07:21 -0700

    component clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { Canvas } from '@react-three/fiber'
 import Button from './components/Button';
 import TextField from './components/TextField';
 import Loader from './components/Loader';
-import { Mesh, MeshBasicMaterial, MeshStandardMaterial } from 'three';
+import { Mesh, MeshBasicMaterial, MeshStandardMaterial, MeshToonMaterial } from 'three';
 import Slider from './components/Slider';
 import Switch from './components/Switch';
-
+import { useState } from 'react';
 
 
 export default function App() {
 
   return (
-    <Canvas shadows>
-  <Switch />
+    <Canvas>
+    <Loader />
     </Canvas>
   )
 }
