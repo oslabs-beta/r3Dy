fd762f48ccbf934a096ac7a4568926214997f3ea
bryan trang <bryanltrang@gmail.com>
2023-07-13 16:31:40 -0700

    app changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,53 +11,20 @@ import Switch2 from './components/Switch2';
 import Switch from './components/Switch';
 import { Perf } from 'r3f-perf'
 import { OrbitControls } from "@react-three/drei"
+type camConfig = {
+ fov: 75 | number,
+ near: 0.1 | number,
+ far: 1000 | number,
+ position: [0, 0, 5] | [number, number, number]
+}
 
 export default function App() {
 
   const [slider, setSlider] = useState(0);
-
   return (
-    <div className='examples'>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <TextField />
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Button/>
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Switch />
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Switch2/>
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Slider /> // need on change
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Loader model={1} scale={1.1}  />
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Loader model={2} scale={1.1} />
-        </Canvas>
-      </div>
-      <div style={{height: "300px"}}>
-        <Canvas shadows>
-          <Loader model={3} scale={1.25}  />
-        </Canvas>
-      </div>
-  </div>
+    <Canvas shadows camera={camConfig}>
+      <OrbitControls/>
+      <Perf/>
+    </Canvas>
   )
 }
