ce2f96e2d7e6dca16c044dbbc29a1dd69d9f0275
Alexander Jessen <alecjessen@gmail.com>
2023-07-19 20:30:48 -0700

    button font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -29,7 +29,7 @@ export default function Button(props: ButtonProps): ReactElement {
     const fontSize = props.fontSize || .5;
     const fontColor = props.fontColor || '#ffffff'
     const handleClick = props.handleClick || undefined;
-
+    const font = props.font || undefined;
     const buttonWidth = text.length * fontSize * 1.1; 
     const buttonHeight = fontSize * 2.7; 
 
@@ -63,7 +63,7 @@ export default function Button(props: ButtonProps): ReactElement {
                 </RoundedBox>
             </mesh>
             <Text 
-                font={font ? font : undefined} 
+                font={font} 
                 fontSize={fontSize} 
                 color={fontColor} 
                 position-z={.3}
